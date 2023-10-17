3fe1d88ddba416a893dcc8004966622c9d8c35f2
Manas <105818439+Manas273@users.noreply.github.com>
2023-10-16 22:43:43 -0600

    Added Review section on testimonial page

--- app/testimonial/page.tsx
+++ app/testimonial/page.tsx
@@ -1,5 +1,17 @@
-const Testimonial = () => {
+import '@/styles/testimonial.css';
+import ReviewPage from '@/app/testimonial/PostReview';
 
+
+
+const Testimonial = () => {
+    return (
+        <><div className='container-fluid py-2'>
+            <h1>What our Clients say</h1>
+        </div>
+        <div>
+                <ReviewPage />
+            </div></>
+    )
 
 
 }
